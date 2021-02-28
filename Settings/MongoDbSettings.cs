e1d0b62fb5b52c9e6f4a68ab87564da9b0fa4f10
Nour Elgafy <Nour-Elgafy@outlook.com>
2021-02-28 20:42:58 +0200

    updated mongodb settings

--- Settings/MongoDbSettings.cs
+++ Settings/MongoDbSettings.cs
@@ -2,13 +2,12 @@ namespace MomentumRegistrationApi.Settings
 {
     public class MongoDbSettings
     {
-        public string Host { get; set; }
+        // public string Host { get; set; }
         public string DbName { get; set; }
-        public int Port { get; set; }
-        public string User { get; set; }
+        // public int Port { get; set; }
+        // public string User { get; set; }
         public string Password { get; set; }
-
-        public string ConnectionString =>$"mongodb://{User}:{Password}@{Host}:{Port}";
+        public string ConnectionString =>$"mongodb+srv://mongoadmin:{Password}@momentumregistrationclu.kkbra.mongodb.net/MomentumRegistration?retryWrites=true&w=majority";
 
     }
 }
